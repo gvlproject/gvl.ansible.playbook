f6f34b4db7a4faabf63cbb72708a8048e606ee4c
nuwan_ag <nuwan.ag@gmail.com>
2015-04-23 04:37:23 +1000

    Fix missing trailing slash in url.

--- roles/gvl.image/files/scripts/triggers/trigger_gvldash_cmdline_install.sh
+++ roles/gvl.image/files/scripts/triggers/trigger_gvldash_cmdline_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X PUT --data '{ "status":"installing" }' http://127.0.0.1/api/v1/packages/gvl_cmdline_utilities
+curl -X PUT --data '{ "status":"installing" }' http://127.0.0.1/api/v1/packages/gvl_cmdline_utilities/
