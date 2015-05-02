cd43ac4c1b3d8bbb12c91dcbaa9b0017c6e3a97d
nuwan_ag <nuwan.ag@gmail.com>
2015-05-03 02:17:36 +1000

    Copied gvl init script and fixed up cmdline util install

--- roles/gvl.image/files/scripts/cmdlineutils/setup_utils_silent.sh
+++ roles/gvl.image/files/scripts/cmdlineutils/setup_utils_silent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 printf "This will download and run scripts to configure GVL command-line utilities, including environment modules, RStudio, and IPython Notebook. Press any key to continue."
 if [ -d "/opt/gvl/gvl_commandline_utilities" ]; then
-   cd /opt/gvl/gvl_commandline_utilities
-   sudo su - ubuntu -c "source run_all.sh -s"
-   sudo su - researcher -c "screen -d -m -S ipython /opt/gvl/scripts/cmdlineutils/ipython_screen.sh"
+   sudo su - ubuntu -c "cd /opt/gvl/gvl_commandline_utilities; source run_all.sh -s"
+   sudo su - researcher -c "cd /opt/gvl/gvl_commandline_utilities; screen -d -m -S ipython ipython_screen.sh"
 fi
