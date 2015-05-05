f93075d84489c96162b2e1e001cb73d9cbc88d48
nuwan_ag <nuwan.ag@gmail.com>
2015-05-06 01:00:11 +1000

    Fixes for commandline util user creation.

--- roles/gvl.image/files/scripts/cmdlineutils/setup_utils_silent.sh
+++ roles/gvl.image/files/scripts/cmdlineutils/setup_utils_silent.sh
@@ -2,5 +2,5 @@
 printf "This will download and run scripts to configure GVL command-line utilities, including environment modules, RStudio, and IPython Notebook. Press any key to continue."
 if [ -d "/opt/gvl/gvl_commandline_utilities" ]; then
    sudo su - ubuntu -c "cd /opt/gvl/gvl_commandline_utilities; source run_all.sh -s"
-   sudo su - researcher -c "cd /opt/gvl/gvl_commandline_utilities; screen -d -m -S ipython ipython_screen.sh"
+   sudo su - researcher -c "screen -d -m -S ipython /opt/gvl/scripts/cmdlineutils/ipython_screen.sh"
 fi
