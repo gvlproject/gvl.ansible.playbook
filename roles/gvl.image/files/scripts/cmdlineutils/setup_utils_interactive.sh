163dccb7f0f6e7d59b655bf87de5001b844ef73a
nuwan_ag <nuwan.ag@gmail.com>
2015-09-23 12:37:18 +1000

    Moved roles into separate repositories for compatibility with Ansible Galaxy

--- roles/gvl.image/files/scripts/cmdlineutils/setup_utils_interactive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-printf "This will download and run scripts to configure GVL command-line utilities, including environment modules, RStudio, and IPython Notebook. Press any key to continue."
-read -n 1 _input
-if [ -d "/opt/gvl/gvl_commandline_utilities" ]; then
-   cd /opt/gvl/gvl_commandline_utilities
-   sh run_all.sh
-fi
-printf "Press any key to close"
-read -n 1 _input
--- roles/gvl.image/files/scripts/cmdlineutils/setup_utils_silent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "This will download and run scripts to configure GVL command-line utilities, including environment modules, RStudio, and IPython Notebook. Press any key to continue."
-if [ -d "/opt/gvl/gvl_commandline_utilities" ]; then
-   sudo su - ubuntu -c "cd /opt/gvl/gvl_commandline_utilities; source run_all.sh -s"
-   sudo su - researcher -c "screen -d -m -S ipython /opt/gvl/scripts/cmdlineutils/ipython_screen.sh"
-fi
